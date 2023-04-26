65e58415e36dd2a66e14b7a63edee82a498bdb0b
Laurence Brown <lifbrown@gmail.com>
2023-04-26 09:29:44 +0300

    Added seconds car. split car source to its own file

---
+++ Car.cs
@@ -0,0 +1,64 @@
+
+namespace OopCar
+{
+    public class Car
+    {
+        private void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+        string maker;
+        string model;
+        float currentSpeed;
+        float topSpeed;
+        bool engineRunninng;
+        public Car(string _maker, string _model, float _topSpeed)
+        {
+            maker = _maker;
+            model = _model;
+            topSpeed = _topSpeed;
+            currentSpeed = 0f;
+            engineRunninng = false;
+        }
+
+        public void StartEngine()
+        {
+            engineRunninng = true;
+            Print(model + " turned engine on.");
+        }
+
+        public void StopEngine()
+        {
+            engineRunninng = false;
+            Print(model + " turned engine off.");
+        }
+
+        public void Accelerate(float amount)
+        {
+            if (!checkEngine())
+            {
+                Print("The Engine is not running!");
+                return;
+            }
+            if (currentSpeed + amount < topSpeed)
+                currentSpeed += amount;
+            else
+                currentSpeed = topSpeed;
+            Print(model + " speed is " + currentSpeed);
+        }
+
+        public void Decelerate(float amount)
+        {
+            if (currentSpeed - amount > 0)
+                currentSpeed -= amount;
+            else
+                currentSpeed = 0;
+            Print(model + " speed is " + currentSpeed);
+        }
+
+        private bool checkEngine()
+        {
+            return engineRunninng;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,8 +10,10 @@ namespace OopCar
         {
             Factory vwFactory = new Factory("Volkswagon");
             Car beetle1 = vwFactory.createCar("Beetle", 50f);
+            Car camper = vwFactory.createCar("Camper", 45f);
 
             TestCar(ref beetle1);
+            TestCar(ref camper);
 
         }
 
@@ -48,69 +50,7 @@ namespace OopCar
     }
 
 
-    public class Car
-    {
-        private void Print(string str)
-        {
-            Console.WriteLine(str);
-        }
-        string maker;
-        string model;
-        float currentSpeed;
-        float topSpeed;
-        bool engineRunninng;
-        public Car(string _maker, string _model, float _topSpeed)
-        {
-            maker = _maker;
-            model = _model;
-            topSpeed = _topSpeed;
-            currentSpeed = 0f;
-            engineRunninng = false;
-        }
-
-        public void StartEngine()
-        {
-            engineRunninng = true;
-            Print("The engine is Running.");
-        }
-
-        public void StopEngine()
-        {
-            engineRunninng = false;
-            Print("The Engine is off.");
-        }
-
-        public void Accelerate(float amount)
-        {
-            if (!checkEngine())
-            {
-                Print("The Engine is not running!");
-                return;
-            }
-            if (currentSpeed + amount < topSpeed)
-                currentSpeed += amount;
-            else
-                currentSpeed = topSpeed;
-            Print(model + " speed is " + currentSpeed);
-        }
-
-        public void Decelerate(float amount)
-        {
-            if (currentSpeed - amount > 0)
-                currentSpeed -= amount;
-            else
-                currentSpeed = 0;
-            Print(model + " speed is " + currentSpeed);
-        }
-
-        private bool checkEngine()
-        {
-            return engineRunninng;
-        }
-
-
-    }
-
+   
 
 
 }
